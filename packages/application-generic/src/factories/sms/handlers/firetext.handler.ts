879197afc61e493f240e42ee6b03ff05725b799c
Nick Prinsloo <nick@ultramed.co>
2022-10-11 19:25:49 +0100

    fix: Add firetext handler to application-generic

---
+++ packages/application-generic/src/factories/sms/handlers/firetext.handler.ts
@@ -0,0 +1,18 @@
+import { FiretextSmsProvider } from '@novu/firetext';
+import { ChannelTypeEnum } from '@novu/shared';
+import { ICredentials } from '@novu/dal';
+import { BaseSmsHandler } from './base.handler';
+
+export class FiretextSmsHandler extends BaseSmsHandler {
+  constructor() {
+    super('firetext', ChannelTypeEnum.SMS);
+  }
+  buildProvider(credentials: ICredentials) {
+    const config: {
+      apiKey: string;
+      from: string;
+    } = { apiKey: credentials.apiKey, from: credentials.from };
+
+    this.provider = new FiretextSmsProvider(config);
+  }
+}
